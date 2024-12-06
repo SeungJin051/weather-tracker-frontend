d73519dccda3c70ccaa4c5cb9d54dcca10ddc0f1
seungjin051 <seungjin051@gmail.com>
2024-12-06 14:51:31 +0000

    rename: 프로젝트 이름 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata = {
     title: 'Weather Tracker',
     description:
       '기후 통계를 한눈에 확인하세요. 월별 온도, 강수량, 기후 패턴 등 상세 데이터를 간편하게 살펴볼 수 있어요.',
-    url: 'https://vercel.com/seungjinlims-projects/tune-tracker-frontend',
     images: [
       {
         url: '/public/weather-illustration.png',
